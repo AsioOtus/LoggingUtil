e3a9b54a8c146fe873dad8299a158bd8e1a0ee12
Asio Otus <asio.otus.verus@icloud.com>
2021-09-29 01:36:21 +0300

    Delete LogConfiguration.swift

--- Sources/LoggingUtil/Models/LogConfiguration.swift
+++
@@ -1,9 +0,0 @@
-protocol LogExporterConfiguration { }
-
-public struct LogConfiguration {
-	let stringValue: String
-	let keyString: [String: String]
-	let keyValue: [String: Any]
-	let converters: [String: LogConverter]
-	let exporterConfigurations: [String: LogExporterConfiguration]
-}
