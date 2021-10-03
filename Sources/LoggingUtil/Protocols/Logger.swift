4954e2824895d3469befc98e13137a60d6afddc0
Asio Otus <asio.otus.verus@icloud.com>
2021-10-03 17:46:01 +0300

    Add default value for configuration

--- Sources/LoggingUtil/Protocols/Logger.swift
+++ Sources/LoggingUtil/Protocols/Logger.swift
@@ -17,35 +17,35 @@ public protocol Logger {
 }
 
 public extension Logger {
-	func trace    (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func trace    (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .trace,    message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func debug    (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func debug    (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .debug,    message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func info     (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func info     (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .info,     message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func notice   (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func notice   (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .notice,   message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func warning  (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func warning  (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .warning,  message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func fault    (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func fault    (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .fault,    message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func error    (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func error    (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .error,    message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 	
-	func critical (_ message: Message, details: Details? = nil, configuration: Configuration?, label: String? = nil, file: String = #fileID, line: Int = #line) {
+	func critical (_ message: Message, details: Details? = nil, configuration: Configuration? = nil, label: String? = nil, file: String = #fileID, line: Int = #line) {
 		log(level: .critical, message: message, details: details, configuration: configuration, label: label, file: file, line: line)
 	}
 }
