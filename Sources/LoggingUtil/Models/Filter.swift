1c713e805c7dc36f06e8518ab863c6d7bef3de32
Asio Otus <asio.otus.verus@icloud.com>
2021-10-03 17:56:00 +0300

    Add Filters

--- Sources/LoggingUtil/Models/Filter.swift
+++ Sources/LoggingUtil/Models/Filter.swift
@@ -1,1 +1,17 @@
 public typealias Filter<Message: Codable, Details: RecordDetails> = (Record<Message, Details>) -> Bool
+
+public enum Filters {
+	public static func whitelist <Message: Codable, Details: RecordDetails> (_ allowedValues: Set<String>) -> Filter<Message, Details> {
+		{ record in
+			guard let filterValue = record.configuration?.keyValue["filter"], allowedValues.contains(filterValue) else { return false }
+			return true
+		}
+	}
+	
+	public static func blacklist <Message: Codable, Details: RecordDetails> (_ deniedValues: Set<String>) -> Filter<Message, Details> {
+		{ record in
+			guard let filterValue = record.configuration?.keyValue["filter"], deniedValues.contains(filterValue) else { return true }
+			return false
+		}
+	}
+}
